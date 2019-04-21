b420666e663824a75728b28534826b9aec6e55ae
Softwarica College Lab <softwarica@softwarica.com>
2019-04-21 11:15:20 +0545

    Added Calculator into the 1st Fragments page

--- app/src/main/java/Fragments/FirstFragment.java
+++ app/src/main/java/Fragments/FirstFragment.java
@@ -1,19 +1,24 @@
 package Fragments;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.e.fragments.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class FirstFragment extends Fragment {
-
+public class FirstFragment extends Fragment implements View.OnClickListener {
+    private Button btnCalculate;
+    private EditText etFirst, etSecond;
 
     public FirstFragment() {
         // Required empty public constructor
@@ -24,7 +29,22 @@ public class FirstFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false);
+        View view = inflater.inflate(R.layout.fragment_first, container, false);
+
+        etFirst = view.findViewById(R.id.etFirst);
+        etSecond = view.findViewById(R.id.etSecond);
+        btnCalculate = view.findViewById(R.id.btnCalculate);
+
+        btnCalculate.setOnClickListener(this);
+        return view;
     }
 
+    @Override
+    public void onClick(View v) {
+    int first = Integer.parseInt(etFirst.getText().toString());
+    int second = Integer.parseInt(etSecond.getText().toString());
+    int result = first + second;
+
+        Toast.makeText(getActivity(), "Sum is : " + result, Toast.LENGTH_LONG).show();
+    }
 }
--- app/src/main/java/com/e/fragments/MainActivity.java
+++ app/src/main/java/com/e/fragments/MainActivity.java
@@ -18,7 +18,7 @@ private Boolean status = true;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnFragment=findViewById(R.id.btnFragement);
+        btnFragment=findViewById(R.id.btnFragment);
         btnFragment.setOnClickListener(this);
     }
 
@@ -31,6 +31,7 @@ private Boolean status = true;
         if (status) {
             FirstFragment firstFragment = new FirstFragment();
             fragmentTransaction.add(R.id.fragmentContainer,firstFragment);
+            fragmentTransaction.addToBackStack(null);
             fragmentTransaction.commit();
             btnFragment.setText("Load Second Fragment");
             status = false;
@@ -38,6 +39,7 @@ private Boolean status = true;
         } else {
             SecondFragment secondFragment = new SecondFragment();
             fragmentTransaction.add(R.id.fragmentContainer,secondFragment);
+            fragmentTransaction.addToBackStack(null);
             fragmentTransaction.commit();
             btnFragment.setText("Load First Fragment");
             status = true;
